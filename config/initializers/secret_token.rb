f69b40c43cdc5588e8f97546db843235403fe40e
mudumh <mvharsha.mail@gmail.com>
2014-02-10 17:10:14 -0700

    Initial Commit. Added index and show actions for article model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '2ba3ae1e0de390c34b404f8e90d4e7ac7d5b74d794fc981f93acd2d1d1fdee8e71657f96546a8a34887b42aea86c5c749d6b16583d010c54fe2815ef833bb86b'
