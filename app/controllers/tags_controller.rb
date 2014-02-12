2b5d6134dd7fa1dd5fd2654b4971cedda930d1ba
mudumh <mvharsha.mail@gmail.com>
2014-02-11 18:48:04 -0700

    completed the tagging feature

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,4 +4,15 @@ class TagsController < ApplicationController
   		@tag = Tag.find(params[:id])
 	end
 	
+	def index
+		@tags = Tag.all
+
+	end
+	def destroy
+		
+		Tag.find(params[:id]).destroy
+		
+		redirect_to tags_path
+		
+	end
 end
